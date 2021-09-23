a2873110746b0c5492dae4b988f6bf9a1286e238
Sandeep Karkala <sandeepkarkala@gmail.com>
2021-09-24 01:00:41 +0530

    Log formatting.

--- buildall.sh
+++ buildall.sh
@@ -45,32 +45,32 @@ mkdir -p $ARTIFACTS_PATH/charts
 mkdir -p $ARTIFACTS_PATH/images
 
 if [[ -n "$CLEAN_BUILD_BUILDER_IMAGE" ]] && [[ "$CLEAN_BUILD_BUILDER_IMAGE" == "yes" ]]; then
-echo -e "\e[1;32;40m[TMAAS-BUILD] Clean Build Builder Image...\e[0m"
+echo -e "[TMAAS-BUILD] Clean Build Builder Image...\e[0m"
 BUILDER_ARG="--no-cache"
 fi
 
-echo -e "\e[1;32;40m[TMAAS-BUILD] Build: $BUILDER_NAME, Version:$BUILDER_VERSION \e[0m"
+echo -e "[TMAAS-BUILD] Build: $BUILDER_NAME, Version:$BUILDER_VERSION"
 docker build --rm $BUILDER_ARG -f ./build/$MICROSERVICE_NAME-builder-dockerfile -t $BUILDER_NAME:$BUILDER_VERSION .
 
 ##NANO SEC timestamp
 BUILDER_LABEL="tmaas-gw-builder-$(date +%s%9N)"
-echo -e "\e[1;32;40m[TMAAS-BUILD] Build MICROSERVICE_NAME:$MICROSERVICE_NAME, Version:$MICROSERVICE_VERSION \e[0m"
+echo -e "[TMAAS-BUILD] Build MICROSERVICE_NAME:$MICROSERVICE_NAME, Version:$MICROSERVICE_VERSION"
 docker build --rm --build-arg BUILDER_LABEL=$BUILDER_LABEL -f ./build/$MICROSERVICE_NAME-dockerfile -t $MICROSERVICE_NAME:$MICROSERVICE_VERSION .
 
-echo -e "\e[1;32;40m[TMAAS-BUILD] Releasing artifacts \e[0m"
+echo -e "[TMAAS-BUILD] Releasing artifacts"
 docker save $MICROSERVICE_NAME:$MICROSERVICE_VERSION | gzip > $ARTIFACTS_PATH/images/$MICROSERVICE_NAME-$MICROSERVICE_VERSION.tar.gz
 
-echo -e "\e[1;32;40m[TMAAS-BUILD] Upating tmaas-gw chart \e[0m"
+echo -e "[TMAAS-BUILD] Upating tmaas-gw chart"
 cp -r ./charts/tmaas-gw $ARTIFACTS_PATH/charts/.
 sed -i -e "s/topo_gw_tag/$1/" $ARTIFACTS_PATH/charts/tmaas-gw/values.yaml
 #sed -i -e "s/cim_tag/$4/" $ARTIFACTS_PATH/charts/tmaas-gw/values.yaml
 md5sum $ARTIFACTS_PATH/images/*
 
-echo -e "\e[1;32;40m[TMAAS-BUILD] Deleting intermediate and microservice images \e[0m"
+echo -e "[TMAAS-BUILD] Deleting intermediate and microservice images"
 docker image prune -f --filter "label=IMAGE-TYPE=$BUILDER_LABEL"
 docker rmi -f $MICROSERVICE_NAME:$MICROSERVICE_VERSION
 
 if [[ -n "$DELETE_BUILDER_IMAGE" ]] && [[ "$DELETE_BUILDER_IMAGE" == "yes" ]]; then
-echo -e "\e[1;32;40m[TMAAS-BUILD] Deleting builder image \e[0m"
+echo -e "[TMAAS-BUILD] Deleting builder image"
 docker rmi -f $BUILDER_NAME:$BUILDER_VERSION
 fi
